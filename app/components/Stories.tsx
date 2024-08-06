49b0d11ce1a168dcc1328df8bcb8eb7bc29540e6
VincentCongDao <congdanhdao95@gmail.com>
2024-08-05 17:19:18 -0700

    Adding stories container

--- app/components/Stories.tsx
+++ app/components/Stories.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+
 const Stories = () => {
-    return (<div>Stories</div>);
+    return (<div className="stories p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs">
+        <div className="flex gap-8">
+            <div className="flex flex-col items-center gap-2 cursor-pointer">
+                <Image src="" alt="" width={80} height={80} className=" rounded-full ring-2" />
+                <span className="text-sm">John Doe</span>
+
+            </div>
+        </div>
+    </div>);
 }
 
 export default Stories;

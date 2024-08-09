2981ff35d629f3add72a8eb812934e5f5b809a3d
VincentCongDao <congdanhdao95@gmail.com>
2024-08-09 01:04:35 -0700

    Adding the template for the left menu in the profile

--- app/components/LeftMenu.tsx
+++ app/components/LeftMenu.tsx
@@ -1,5 +1,66 @@
-const LeftMenu = () => {
-    return (<div>LeftMenu</div>);
+import Image from "next/image";
+import Link from "next/link";
+import Ad from "./Ad";
+import ProfileCard from "./ProfileCard";
+
+const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
+    return (
+        <div className="flex flex-col gap-6">
+            {type === "home" && <ProfileCard />}
+            <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/posts.png`} alt="image" width={20} height={20} />
+                    <span>My Post</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/activity.png`} alt="image" width={20} height={20} />
+                    <span>Activity</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/market.png`} alt="image" width={20} height={20} />
+                    <span>Marketplace</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/events.png`} alt="image" width={20} height={20} />
+                    <span>Events</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/albums.png`} alt="image" width={20} height={20} />
+                    <span>Albums</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/videos.png`} alt="image" width={20} height={20} />
+                    <span>My Post</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/news.png`} alt="image" width={20} height={20} />
+                    <span>News</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/coures.png`} alt="image" width={20} height={20} />
+                    <span>Courses</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/lists.png`} alt="image" width={20} height={20} />
+                    <span>Lists</span>
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+                <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
+                    <Image src={`/settings.png`} alt="image" width={20} height={20} />
+                    Settings
+                </Link>
+                <hr className="border-t-1 border-gray-50 w-36 self-center" />
+            </div>
+            <Ad size="sm" />
+        </div>);
 }
 
 export default LeftMenu;
